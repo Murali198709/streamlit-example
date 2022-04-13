ee956f4252624fed10013a3d56e17566c3703537
muralitulluri <Murali.Krishna.Tulluri@smu.ca>
2022-04-13 19:04:26 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,6 @@ JSONContent = requests.get(url).json()
 bitcoin_prices = pd.DataFrame(JSONContent['prices'],columns=['date','price'])
 
 
-#print(bitcoin_prices.count())
-
 bitcoin_prices['date'] = pd.to_datetime(bitcoin_prices['date'],unit='ms')
 
 Count = bitcoin_prices['date'].nunique()

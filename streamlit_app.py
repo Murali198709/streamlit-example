23a48a9ab866e62451c73ed8df5d16160938a1ac
muralitulluri <Murali.Krishna.Tulluri@smu.ca>
2022-04-13 17:31:57 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ df=bitcoin_prices.rename(columns={'date':'index'}).set_index('index')
 if Currency == 'CAD':
         
     
-    st.line_chart(df[["date","CAD_price"]])
+    st.line_chart(df[["index","CAD_price"]])
     st.write('Average price during this period was '+str(bitcoin_prices['CAD_price'].mean()))
     
 
